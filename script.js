f23825a0bfc201bf310d6f0716f7bc22db0faec7
Keith147 <43358418+Keith147@users.noreply.github.com>
2019-01-14 16:48:30 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#yeet").click(function(){
+ $("#big").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("#slide").click(function(){
+ $("#yoink").animate({width:'toggle'},350)();
+});
+
+
+
+//Spell #3
+
+$("#chun").click(function(){
+ $("#chunga").toggle();
+});
+
+
+
